b3d032e54d7fb83af2bd6f846aab536d02fa9e6e
Dave Charnuska <davecharnuska@gmail.com>
2015-11-12 11:02:40 -0500

    basic handling for break and session length settings

--- js/main.js
+++ js/main.js
@@ -1,3 +1,52 @@
+var breakLength = 5;
+var sessionLength = 25;
+var paused = false
+
 $( document ).ready(function() {
-    console.log( "ready!" );
+	function updateBreakLength() {
+		$(".break-time").text(breakLength);
+	}
+	
+	function updateSessionLength() {
+		$(".session-time").text(sessionLength);
+	}
+	
+	$(".break-red").click(function(){
+		if (paused) {
+			return null;
+		}
+		if (breakLength > 0) {
+			breakLength -= 1;
+			updateBreakLength();
+		}
+	});
+	
+	$(".break-add").click(function(){
+		if (paused) {
+			return null;
+		}
+		breakLength += 1;
+		updateBreakLength();
+	})
+	
+	$(".session-red").click(function(){
+		if (paused) {
+			return null;
+		}
+		if (sessionLength > 0) {
+			sessionLength -= 1;
+			updateSessionLength();
+		}
+	});
+	
+	$(".session-add").click(function(){
+		if (paused) {
+			return null;
+		}
+		sessionLength += 1;
+		updateSessionLength();
+	});
+	
+	updateBreakLength();
+	updateSessionLength();
 });
