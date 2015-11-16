54c27ce2f86ccb0e408e7a58e8d1858b76635433
Dave Charnuska <davecharnuska@gmail.com>
2015-11-16 13:07:37 -0500

    start and switch functionality

--- js/main.js
+++ js/main.js
@@ -1,57 +1,140 @@
-var breakLength = 1;
-var sessionLength = 1;
-var curSessionLength = sessionLength;
-var curBreakLength = breakLength;
+var breakLen = 1;
+var sessionLen = 1;
 var paused = true;
 var sessionRunning = false;
-var breakRunning = false;
+var breakRunning = true;
 
 function minToSecString(min) {
 	var sec = min * 60;
 	var final = String(sec) + "s";
-	console.log(final);
 	return final;
 }
 
-function formatTime(h, m, s) {
-	console.log(h);
+function formatTime(time) {
+    time = time / 10;
+    var min = parseInt(time / 6000),
+        sec = parseInt(time / 100) - (min * 60),
+        hundredths = pad(time - (sec * 100) - (min * 6000), 2);
+    return (min > 0 ? pad(min, 2) : "00") + ":" + pad(sec, 2) + ":" + hundredths;
 }
 
 $( document ).ready(function() {
-	function sessionBreakSwitch() {
+	function updateBreakLen() {
+		$(".break-time").text(breakLen);
+	}
+	
+	function updateSessionLen() {
+		$(".session-time").text(sessionLen);
+	}
+	
+	function newTimer(len, pause) {
 		$('.time').timer('remove');
+		$('.time').timer({
+			duration: minToSecString(len),
+			format: '%M:%S',
+			callback: sessionBreakSwitch
+		});
 		
-		if (sessionRunning) {
-			$('.time').timer({
-				duration: minToSecString(curBreakLength)
-		} else {
-			
+		if (pause) {
+			$('.time').timer('pause');
 		}
 		
+		paused = pause;
 		sessionRunning = !sessionRunning;
 		breakRunning = !breakRunning;
 	}
 	
-	function newTimer () {
+	function minToSec(min) {
+		return min * 60;
+	}
+	
+	$(".break-red").click(function(){
+		if (!paused) {
+			return null;
+		}
+		if (breakLen > 0) {
+			breakLen -= 1;
+			updateBreakLen();
+		}
+	});
+	
+	$(".break-add").click(function(){
+		if (!paused) {
+			return null;
+		}
+		breakLen += 1;
+		updateBreakLen();
+	})
+	
+	$(".session-red").click(function(){
+		if (!paused) {
+			return null;
+		}
+		if (sessionLen > 0) {
+			sessionLen -= 1;
+			updateSessionLen();
+			
+			if (sessionRunning) {
+				newSession();
+			}			
+		}
 		
+	});
+	
+	$(".session-add").click(function(){
+		if (!paused) {
+			return null;
+		}
+		sessionLen += 1;
+		updateSessionLen();
+		
+		if (sessionRunning) {
+			newSession();
+		}	
+	});
+	
+	updateBreakLen();
+	updateSessionLen();
+	newTimer(sessionLen, true);
+	
+	function switchTitle() {
+		if (breakRunning) {
+			$(".session-break").text("Session")
+		} else {
+			$(".session-break").text("Break")
+		}
+	}
+	
+	function sessionBreakSwitch() {
+		switchTitle();
+		if (sessionRunning) {
+			newTimer(breakLen, false)
+		} else {
+			newTimer(sessionLen, false)
+		}
+	}
+	
+	function resetTimer (len) {
+		$('.time').timer('remove');
+		$('.time').timer({
+			duration: minToSecString(len),
+			format: '%M:%S',
+			callback: sessionBreakSwitch
+		});
 	}
 	
 	$(".session").click(function (){
-		if (!sessionRunning) {
-			sessionRunning = true;
-			$('.time').timer({
-				duration: minToSecString(curSessionLength),
-				format: '%M:%S',
-				callback: function() {
-				        
-				    }
-			});
+		if (!sessionRunning && !breakRunning) {
+			// sessionRunning = true;
+			// resetTimer(sessionLen);
+			// paused = false;
+			newTimer(sessionLen, false);
 		} else if (paused) {
+			paused = !paused;
 			$('.time').timer('resume');
 		} else {
+			paused = !paused;
 			$('.time').timer('pause');
 		}
-		
-		paused = !paused;
 	})
 });
