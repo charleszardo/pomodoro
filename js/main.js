1a5046822213e03655a6e05fc704e148b2a13e6a
Dave Charnuska <davecharnuska@gmail.com>
2015-12-11 17:49:47 -0500

    implement clock switching between session and break

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-var breakLen = 1;
-var sessionLen = 1;
+var breakLen = 0.05;
+var sessionLen = 0.05;
 var paused = true;
-var sessionRunning = false;
+var sessionRunning = true;
 var breakRunning = false;
 
 function formatTime(time) {
@@ -12,19 +12,41 @@ function formatTime(time) {
     return (min > 0 ? pad(min, 2) : "00") + ":" + pad(sec, 2) + ":" + hundredths;
 }
 
+function minToMilSec(min) {
+	return min * 60 * 1000;
+}
+
+function timeToMilSec(time) {
+	time = time.split(":")
+	var hr = Number(time[0]);
+	var min = Number(time[1]);
+	var sec = Number(time[2]);
+	return ((hr * 60 + min) * 60 + sec) * 1000;
+}
+
+function updateBreakLen() {
+	$(".break-time").text(breakLen);
+}
+
+function updateSessionLen() {
+	$(".session-time").text(sessionLen);
+}
+
+function switchClocks() {
+	var $bt = $('#breakTimer');
+	var $st = $('#sessionTimer');
+	
+	if (breakRunning) {
+		$bt.hide();
+		$st.show();
+	} else {
+		$st.hide();
+		$bt.show();
+	}
+}
+
 $( document ).ready(function() {
-	function updateBreakLen() {
-		$(".break-time").text(breakLen);
-	}
-	
-	function updateSessionLen() {
-		$(".session-time").text(sessionLen);
-	}
-	
-	function minToMilSec(min) {
-		return min * 60 * 1000;
-	}
-	
+	// functionality to change break and session lengths
 	$(".break-red").click(function(){
 		if (!paused) {
 			return null;
@@ -34,7 +56,7 @@ $( document ).ready(function() {
 			updateBreakLen();
 		}
 	});
-	
+
 	$(".break-add").click(function(){
 		if (!paused) {
 			return null;
@@ -42,7 +64,7 @@ $( document ).ready(function() {
 		breakLen += 1;
 		updateBreakLen();
 	})
-	
+
 	$(".session-red").click(function(){
 		if (!paused) {
 			return null;
@@ -50,71 +72,31 @@ $( document ).ready(function() {
 		if (sessionLen > 0) {
 			sessionLen -= 1;
 			updateSessionLen();
-			
+
 			if (sessionRunning) {
 				newSession();
-			}			
+			}
 		}
-		
+
 	});
-	
+
 	$(".session-add").click(function(){
 		if (!paused) {
 			return null;
 		}
 		sessionLen += 1;
 		updateSessionLen();
-		
+
 		if (sessionRunning) {
 			newSession();
-		}	
-	});
-	
-	function newTimer(len, pause) {
-		var curTime = new Date().getTime();
-		len = minToMilSec(len) + curTime;
-		$clock = $('#clock');
-
-		$clock.countdown(len, function(event) {
-	    $(this).html(event.strftime('%H:%M:%S'));
-	  });
-		
-		if (pause) {
-			$clock.countdown('pause');
+		} else {
+			// newRunner(minToMilSec(sessionLen));
 		}
-	}
-	
-	function timeToMilSec(time) {
-		time = time.split(":")
-		var hr = Number(time[0]);
-		var min = Number(time[1]);
-		var sec = Number(time[2]);
-		return ((hr * 60 + min) * 60 + sec) * 1000;
-	}
-	
-	console.log(timeToMilSec("00:01:00"));
-	
-	function replaceTime() {
-		var time = $clock.text();
-		var ms = timeToMilSec(time);
-		var clock = $("#clock");
-		clock.countdown(ms, function(event) {
-	    $(this).html(event.strftime('%H:%M:%S'));
-	  });
-	}
-	
-	$clock = $('#clock');
-	var curTime = new Date().getTime();
-	var cdLen = minToMilSec(sessionLen) + curTime;
-	$clock.countdown(cdLen, function(event) {
-    $(this).html(event.strftime('%H:%M:%S'));
-  });
-	
-	$clock.countdown('stop');
-	
+	});
+
 	updateBreakLen();
 	updateSessionLen();
-	
+
 	function switchTitle() {
 		if (breakRunning) {
 			$(".session-break").text("Session")
@@ -122,29 +104,60 @@ $( document ).ready(function() {
 			$(".session-break").text("Break")
 		}
 	}
-	
+
 	function sessionBreakSwitch() {
-		switchTitle();
+		switchClocks();
 		if (sessionRunning) {
-			newTimer(breakLen, false)
+			$('#breakTimer').runner('reset');
+			$('#breakTimer').runner('start');
 		} else {
-			newTimer(sessionLen, false)
+			$('#sessionTimer').runner('reset');
+			$('#sessionTimer').runner('start');
 		}
+		
+		sessionRunning = !sessionRunning;
+		breakRunning = !breakRunning;
+		// switchTitle();
+		// if (sessionRunning) {
+		// 	newTimer(breakLen, false)
+		// } else {
+		// 	newTimer(sessionLen, false)
+		// }
 	}
 	
-	$(".session").click(function (){
-		if (!sessionRunning && !breakRunning) {
-			replaceTime();
-			// $clock.countdown('start');
-			sessionRunning = true;
-			breakRunning = false;
-			paused = false;
-		} else if (paused) {
+	function newSessionTimer(time) {
+		$('#sessionTimer').runner({
+			countdown: true,
+	    startAt: time,
+			stopAt: 0
+		}).on('runnerStop', function(eventObject, info) {
+			sessionBreakSwitch();
+		});
+	}
+	
+	function newBreakTimer(time) {
+		$('#breakTimer').runner({
+			countdown: true,
+	    startAt: time,
+			stopAt: 0
+		}).on('runnerStop', function(eventObject, info) {
+			sessionBreakSwitch();
+		});
+	}
+	
+	newSessionTimer(minToMilSec(sessionLen));
+	newBreakTimer(minToMilSec(breakLen));
+	
+	$('#breakTimer').hide();
+	
+	$('#sessionTimer').click(function() {
+	    $('#sessionTimer').runner('toggle');
 			paused = !paused;
-			$clock.countdown('start');
-		} else {
+	});
+	
+	$('#breakTimer').click(function() {
+	    $('#breakTimer').runner('toggle');
 			paused = !paused;
-			$clock.countdown('stop');
-		}
-	})
+	});
 });
+
